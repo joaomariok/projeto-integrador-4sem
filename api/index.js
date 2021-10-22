4e258e8c6ee499fdc74322ca0de43b5671dbc753
Joao Magalhaes <joaomariomag@gmail.com>
2021-10-22 11:12:47 -0300

    Fix api not working when creating database

--- api/index.js
+++ api/index.js
@@ -11,20 +11,63 @@ app.use(express.urlencoded({ extended: false }))
 
 // Connection to mysql database
 const mysql = require("mysql");
-const connection = mysql.createConnection({
-    host: keys.mysqlHost,
-    user: keys.mysqlUser,
-    password: keys.mysqlPass,
-    database: keys.mysqlName,
-    port: keys.mysqlPort
-});
 
-var isConnected = false;
+function createConnectionToDatabase() {
+    return mysql.createConnection({
+        host: keys.mysqlHost,
+        user: keys.mysqlUser,
+        password: keys.mysqlPass,
+        database: keys.mysqlName,
+        port: keys.mysqlPort
+    });
+}
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected to database!");
-    isConnected = true;
+let connection = createConnectionToDatabase();
+let isConnected = false;
+
+const connectInterval = setInterval(() => {
+    if (!isConnected) {
+        connection.connect((err) => {
+            if (err) {
+                console.log(`Waiting for connection: ${err}`);
+                isConnected = false;
+                connection.end();
+                connection = createConnectionToDatabase();
+            }
+            else {
+                console.log("Connected to database!");
+                isConnected = true;
+                clearInterval(connectInterval);
+            }
+        });
+    }
+}, 10000);
+
+app.post("/retry-connection", (req, res) => {
+    const { password } = req.body;
+
+    if (password != keys.mysqlPass) {
+        return res.status(401).send({ 
+            Error: "Wrong database password" 
+        });
+    }
+
+    connection.end();
+    connection = createConnectionToDatabase();
+    connection.connect((err) => {
+        if (err) {
+            console.log(`Couldn't connect to database: ${err}`);
+            isConnected = false;
+        }
+        else {
+            console.log("Connected to database!");
+            isConnected = true;
+        }
+    })
+
+    return res.status(isConnected ? 200 : 503).send({
+        "IsConnected": isConnected,
+    });
 });
 
 // Express route handlers
